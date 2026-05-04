5980911dcba72cc8f9815757ab95f3d4fc1ab75d
priya67803 <www.priya6780@gmail.com>
2026-05-04 20:57:55 +0530

    Updated message for faculty demo

--- test_main.py
+++ test_main.py
@@ -1,14 +1,9 @@
 from fastapi.testclient import TestClient
-from main import app
+from api.test_main import app
 
 client = TestClient(app)
 
-def test_read_root():
+def test_home():
     response = client.get("/")
     assert response.status_code == 200
-    assert response.json() == {"message": "Hello from CI/CD Pipeline on Vercel!"}
-
-def test_health_check():
-    response = client.get("/health")
-    assert response.status_code == 200
-    assert response.json() == {"status": "ok"}
+    assert response.json() == {"message": "CICD API working"}
